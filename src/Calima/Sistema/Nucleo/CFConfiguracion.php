3b638d40b5364e485dc3b65eba5f20d9a120dc9f
Ramón Serrano <ramon.calle.88@gmail.com>
2015-04-18 19:58:56 -0430

    Agregada clase CFConfiguracion con funcion de init para iniciar todas las Constantes de CF.

--- src/Calima/Sistema/Nucleo/CFConfiguracion.php
+++ src/Calima/Sistema/Nucleo/CFConfiguracion.php
@@ -23,82 +23,94 @@
  * <http://www.calimaframework.com>.
  */
 
+namespace Calima\Sistema\Nucleo;
 
-/* inicio la configuracion*/
-define('Cf_CONFIG_INICIO', 'false');
+/**
+ * @author Ramon Serrano <ramon.calle.88@gmail.com>
+ */
+class CFConfiguracion
+{
+    
+    static function init()
+    {
+        /* inicio la configuracion*/
+        define('Cf_CONFIG_INICIO', 'false');
 
-/* Defino zona horaria*/
-date_default_timezone_set('America/Bogota');
+        /* Defino zona horaria*/
+        date_default_timezone_set('America/Bogota');
 
-/* Define una cuenta de correo para uso del app */
-define('DESTINO_EMAIL', 'info@calimaframework.com');
+        /* Define una cuenta de correo para uso del app */
+        define('DESTINO_EMAIL', 'info@calimaframework.com');
 
-/* Defino el formato de fecha */
-define('Cf_FORMATO_FECHA', 'l, d F Y');
+        /* Defino el formato de fecha */
+        define('Cf_FORMATO_FECHA', 'l, d F Y');
 
-/* CalimaFramework clave de licencia */
-define('Cf_LICENSE', 'Cf-O17N-JHK8-TDJL-B5AO-8WKA');
+        /* CalimaFramework clave de licencia */
+        define('Cf_LICENSE', 'Cf-O17N-JHK8-TDJL-B5AO-8WKA');
 
-/* mensajes de warnin */
-define('CF_DEBUG', TRUE);
+        /* mensajes de warnin */
+        define('CF_DEBUG', TRUE);
 
-/* Idioma local */
-define('CF_LOCALE', 'es_ES');
+        /* Idioma local */
+        define('CF_LOCALE', 'es_ES');
 
-/* Create una cuenta con google analytics y agrega el UA en la constante */
-define('CF_ANALYTICS', 'UA-xxxxxx');
+        /* Create una cuenta con google analytics y agrega el UA en la constante */
+        define('CF_ANALYTICS', 'UA-xxxxxx');
 
-/* con la siguiente constante podras crear una ip fija de tu empresa para hacer 
-* pruebas en tu entorno de red basado en tu ip que te ofrece tu proveedor de servicio
-*/
-define('Cf_IPPRUEBAS', 'x.x.x.x');
+        /* con la siguiente constante podras crear una ip fija de tu empresa para hacer 
+        * pruebas en tu entorno de red basado en tu ip que te ofrece tu proveedor de servicio
+        */
+        define('Cf_IPPRUEBAS', 'x.x.x.x');
 
-/* Transladamos a formato local */
-define('CF_FECHA_FORMATO_LOCAL', '%A, %d %B %G');
+        /* Transladamos a formato local */
+        define('CF_FECHA_FORMATO_LOCAL', '%A, %d %B %G');
 
-/* Translatable time format */
-define('CF_LOCALE_TIME_FORMAT', ' %T');
+        /* Translatable time format */
+        define('CF_LOCALE_TIME_FORMAT', ' %T');
 
-/* Por defecto almacenamos los datos de la aplicacion. */
-define('CF_PATH_DATA', dirname(__FILE__) . '/Cf-data');
+        /* Por defecto almacenamos los datos de la aplicacion. */
+        define('CF_PATH_DATA', dirname(__FILE__) . '/Cf-data');
 
-#Configuracion Basica
+        #Configuracion Basica
 
-/* La siguiente CONSTANTE permite el apuntapiento para archivos js, css, imagenes desde la vista hacia el directorio _public */
-define('Cf_BASE_URL', 'http://localhost/Calima-master/');
+        /* La siguiente CONSTANTE permite el apuntapiento para archivos js, css, imagenes desde la vista hacia el directorio _public */
+        define('Cf_BASE_URL', 'http://localhost/Calima-master/');
 
-/* definimos un controlador inicial en nuestro proyecto */
-define('CONTROLADOR_INICIAL', 'instalador');
+        /* definimos un controlador inicial en nuestro proyecto */
+        define('CONTROLADOR_INICIAL', 'instalador');
 
-/* Sedefine una CONSTANTE al directorio adicionales en la vista */
-define('ADICIONALES_VISTA', 'adicionales');
+        /* Sedefine una CONSTANTE al directorio adicionales en la vista */
+        define('ADICIONALES_VISTA', 'adicionales');
 
-/* Definimos una CONSTANTE como nombre de aplicacion */
-define('CF_AP_NOMBRE', 'CalimaFramework');
+        /* Definimos una CONSTANTE como nombre de aplicacion */
+        define('CF_AP_NOMBRE', 'CalimaFramework');
 
-/* Definimos un Slogan para la aplicacion web */
-define('CF_AP_SLOGAN', 'Tu Framework php MVC hispano ');
+        /* Definimos un Slogan para la aplicacion web */
+        define('CF_AP_SLOGAN', 'Tu Framework php MVC hispano ');
 
-/* Empresa de la aplicacion */
-define('CF_AP_EMPRESA', 'www.webcol.net');
+        /* Empresa de la aplicacion */
+        define('CF_AP_EMPRESA', 'www.webcol.net');
 
-/* Creditos de la aplicacion */
-define('CF_AP_CREDITOS', 'Licencia MIT 2014 - 2015 Debeloped by www.webcol.net');
+        /* Creditos de la aplicacion */
+        define('CF_AP_CREDITOS', 'Licencia MIT 2014 - 2015 Debeloped by www.webcol.net');
 
-#webcol seguridad
+        #webcol seguridad
 
-/* Definimos un indice de clave para concatenar en encriptacion de datos */
-define('Cf_KEY_MD5', 'P0L1');
+        /* Definimos un indice de clave para concatenar en encriptacion de datos */
+        define('Cf_KEY_MD5', 'P0L1');
 
-/*  en el controlador concatena la constante con el llamado a la funcion generarCadenaAleatoria() de Cf_PHPSeguridad */
-define('Cf_CSRF_SECRET','Cfbeta');
+        /*  en el controlador concatena la constante con el llamado a la funcion generarCadenaAleatoria() de Cf_PHPSeguridad */
+        define('Cf_CSRF_SECRET','Cfbeta');
 
-/* #base de datos */
+        /* #base de datos */
 
-/* Configuracion de tu base de datos */
-define('CF_BD_HOST', '');
-define('CF_BD_NOMBRE', '');
-define('CF_BD_USUARIO', '');
-define('CF_BD_CLAVE', '');
-define('CF_BD_CHAR', 'utf8');
-define('CF_BD_CONECTOR', 'mysql');
+        /* Configuracion de tu base de datos */
+        define('CF_BD_HOST', '');
+        define('CF_BD_NOMBRE', '');
+        define('CF_BD_USUARIO', '');
+        define('CF_BD_CLAVE', '');
+        define('CF_BD_CHAR', 'utf8');
+        define('CF_BD_CONECTOR', 'mysql');
+    }
+    
+}
